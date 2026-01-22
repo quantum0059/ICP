b825c71e7bb269f188f39cf188c87abc5391a6d5
quantum0059 <157235980+quantum0059@users.noreply.github.com>
2026-01-22 15:25:29 +0530

    Time: 2 ms (77.55%) | Memory: 45 MB (14.23%) - LeetSync

---
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
@@ -0,0 +1,73 @@
+class Solution {
+    static class Node{
+        int value;
+        Node next;
+        Node(int value){
+            this.value = value;
+            this.next = null;
+        }
+    }
+    public int minimumPairRemoval(int[] nums) {
+        if(nums.length == 1) return 0;
+       boolean flag = true;
+       for(int i=1;i<nums.length;i++){
+         if(nums[i-1]>nums[i]){
+             flag = false;
+         }
+       }
+
+       if(flag) return 0;
+
+    //    PriorityQueue<Integer> pq = new PriorityQueue<>();
+       Node newList  = new Node(0);
+       Node dummy = newList;
+
+       for(int i=0;i<nums.length;i++){
+          dummy.next = new Node(nums[i]);
+          dummy = dummy.next;
+       }
+       int steps = 0;
+       while (true) {
+        Node prev = newList;
+        Node curr = newList.next;
+        Node bestPrev = null;
+        int minSum = Integer.MAX_VALUE;
+
+        while (curr != null && curr.next != null) {
+            int s = curr.value + curr.next.value;
+            if (s < minSum) {
+                minSum = s;
+                bestPrev = prev;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
+
+        Node after = bestPrev.next.next.next;
+        bestPrev.next = new Node(minSum);
+        bestPrev.next.next = after;
+
+        steps++;
+        if (isSorted(newList)) break;
+  }
+
+
+       return steps;
+
+    }
+
+    static boolean isSorted(Node newList){
+        Node dummy = newList.next.next;
+        Node prev = newList.next;
+
+        while(dummy!=null){
+            if(prev.value > dummy.value){
+                return false;
+            }
+            prev = prev.next;
+            dummy = dummy.next;
+        }
+
+        return true;
+    }
+}
