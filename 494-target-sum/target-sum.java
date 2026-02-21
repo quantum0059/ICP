075e7aa88854970a95191fcf6c245a99006fe58e
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-02-21 23:52:33 +0530

    Time: 4 ms (86.77%) | Memory: 45.2 MB (43.31%) - LeetSync

---
+++ 494-target-sum/target-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        int totalSum =0;
+        for(int i: nums){
+            totalSum+=i;
+        }
+
+        if(Math.abs(target) > totalSum) return 0;
+        if((target+totalSum)%2 != 0) return 0;
+        //p-n = target;
+        //p+n = totalSum;
+        int p = (totalSum+target)/2;
+        int[][] dp = new int[n][p+1];
+
+        for(int[] arr: dp){
+            Arrays.fill(arr, -1);
+        }
+
+        return helper(nums, dp, 0, p);
+    }
+    static int helper(int[] nums, int[][] dp, int idx , int rem){
+        if(idx == nums.length) return rem==0?1:0;
+        if(dp[idx][rem] != -1) return dp[idx][rem];
+
+        int stillValid = helper(nums, dp, idx+1, rem);
+        if(nums[idx]<=rem){
+            stillValid += helper(nums, dp, idx+1, rem-nums[idx]);
+        }
+
+        return dp[idx][rem] = stillValid;
+    } 
+}
