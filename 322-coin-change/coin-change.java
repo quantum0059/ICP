e4e03aa558950058d22a02847bae926ffca7c13c
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-02-21 20:59:43 +0530

    Time: 16 ms (66.47%) | Memory: 47.1 MB (31.12%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,30 +1,34 @@
 class Solution {
     static final int INF = (int)1e9;
+
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
 
-        int[][] dp = new int[n][amount+1];
-
-        for(int[] arr: dp){
-            Arrays.fill(arr, -1);
+        // initialize
+        for (int j = 1; j <= amount; j++) {
+            dp[n][j] = INF;
         }
 
-        int ans =  helper(coins, dp, amount, 0);
-        return ans == INF ? -1:ans;
-    }
-
-    static int helper(int[] coins, int[][] dp, int sum, int idx){
-        if(sum<0) return INF;
-        if(sum == 0) return 0;
-        if(idx==coins.length){
-            return INF;
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
         }
-        if(dp[idx][sum] != -1) return dp[idx][sum];
-       
-        int  pick = 1+ helper(coins, dp, sum-coins[idx], idx);
-       
-        int notPick = helper(coins, dp, sum, idx+1);
 
-        return dp[idx][sum] = Math.min(pick,notPick);
+        // fill table
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int sum = 1; sum <= amount; sum++) {
+
+                int notPick = dp[idx + 1][sum];
+                int pick = INF;
+
+                if (coins[idx] <= sum) {
+                    pick = 1 + dp[idx][sum - coins[idx]];
+                }
+
+                dp[idx][sum] = Math.min(pick, notPick);
+            }
+        }
+
+        return dp[0][amount] >= INF ? -1 : dp[0][amount];
     }
 }
