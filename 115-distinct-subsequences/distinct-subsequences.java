3b46695a72a9c8a89ea1bcc26e8743b6ef8bfcc0
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-02-26 20:45:25 +0530

    Time: 20 ms (53.91%) | Memory: 54.4 MB (63.06%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -3,23 +3,21 @@ class Solution {
         int n = s.length();
         int m = t.length();
 
-        int[][] dp = new int[n][m];
-        for(int[] arr: dp){
-            Arrays.fill(arr, -1);
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
         }
 
-        return helper(s, t, n-1, m-1, dp);
-    }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] =  dp[i-1][j-1]+dp[i-1][j];
+                } else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
 
-    static int helper(String s, String t, int i, int j, int[][] dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
-        
-        if(dp[i][j]!= -1) return dp[i][j];
-        if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = (helper(s, t, i-1, j-1, dp)+helper(s, t, i-1, j, dp));
-        } 
-
-        return dp[i][j] = helper(s, t, i-1, j, dp);
+        return dp[n][m];
     }
 }
