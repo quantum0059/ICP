8d088db68b300809949960b8203ff07654f30ff4
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-02-28 21:09:12 +0530

    Time: 19 ms (39.36%) | Memory: 70.5 MB (44.42%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,34 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
+        int[][] dp = new int[n+1][2];
 
-        for(int[] arr: dp){
-            Arrays.fill(arr, -1);
-        } 
+        for(int i=n-1;i>=0;i--){
 
-        return helper(prices, fee, 1, 0, dp);
-    }
 
-    static int helper(int[] prices, int fee, int buy, int idx, int[][] dp){
-        if(idx == prices.length) return 0;
+            dp[i][1] = Math.max((-prices[i]+dp[i+1][0]), dp[i+1][1]);
 
-        if(dp[idx][buy] != -1) return dp[idx][buy];
-
-        int profit = 0;
-
-        if(buy == 1){
-            int take = -prices[idx]+helper(prices, fee, 0, idx+1, dp);
-            int skip = helper(prices, fee, 1, idx+1, dp);
-
-            profit = Math.max(take, skip);
-        }else{
-            int take = prices[idx]-fee+helper(prices, fee, 1, idx+1, dp);
-            int skip = helper(prices, fee, 0, idx+1, dp);
-
-            profit = Math.max(take, skip);
+            dp[i][0] = Math.max((-fee+prices[i]+dp[i+1][1]), dp[i+1][0]);
         }
 
-        return dp[idx][buy] = profit;
+        return dp[0][1];
     }
 }
