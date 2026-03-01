7568a1bba424df6a84d4ffc42db7f8b59a1bb96f
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-03-01 20:20:56 +0530

    Time: 27 ms (9.11%) | Memory: 45.7 MB (22.73%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,43 +1,29 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] length = new int[n];
-        int[] count = new int[n];
-        int maxLength = 0;
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            calculateDP(i, nums, length, count);
-            maxLength = Math.max(maxLength, length[i]);
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (length[i] == maxLength) {
-                result += count[i];
-            }
-        }
-
-        return result;
-    }
-
-    private void calculateDP(int i, int[] nums, int[] length, int[] count) {
-        if (length[i] != 0) {
-            return;
-        }
-
-        length[i] = 1;
-        count[i] = 1;
-
-        for (int j = 0; j < i; j++) {
-            if (nums[j] < nums[i]) {
-                calculateDP(j, nums, length, count);
-                if (length[j] + 1 > length[i]) {
-                    length[i] = length[j] + 1;
-                    count[i] = 0;
-                }
-                if (length[j] + 1 == length[i]) {
-                    count[i] += count[j];
+        int[] dp = new int[nums.length];
+        int[] cnt = new int[nums.length];
+        Arrays.fill(dp, 1);
+        Arrays.fill(cnt, 1);
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j] && dp[i] < dp[j]+1){
+                    dp[i] = dp[j]+1;
+                    cnt[i] = cnt[j];
+                }else if(dp[i] == dp[j]+1){
+                    cnt[i]+=cnt[j];
                 }
             }
         }
+
+        int max = Arrays.stream(dp).max().getAsInt();
+        int count =0;
+
+        for(int i=0;i<nums.length;i++){
+            if(dp[i] == max){
+                count+=cnt[i];
+            }
+        }
+
+        return count;
     }
 }
