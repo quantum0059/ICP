0fa348c1c9eab77e02e6a0e45747785e0bf62d52
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-03-06 14:43:38 +0530

    Time: 1 ms (99.83%) | Memory: 43.8 MB (37.69%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,27 +1,29 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int n = s.length();
-        int m = p.length();
+        
+        Boolean[][] dp = new Boolean[s.length()+1][p.length()+1];
 
-        return helper(s, p, 0, 0);
+        return helper(s, p, 0, 0, dp);
     }
 
-    static boolean helper(String s, String p, int idx1, int idx2){
+    static boolean helper(String s, String p, int idx1, int idx2, Boolean[][] dp){
        
         if(idx2>=p.length()){
             return idx1>=s.length();
         } 
 
+        if(dp[idx1][idx2] != null) return dp[idx1][idx2];
+
         boolean match = (idx1<s.length()) && (s.charAt(idx1) == p.charAt(idx2) || p.charAt(idx2) == '.');
 
         if(idx2+1<p.length() && p.charAt(idx2+1)=='*'){
-           return (helper(s, p, idx1, idx2+2) || (match && helper(s, p, idx1+1, idx2)));
+           return dp[idx1][idx2] = (helper(s, p, idx1, idx2+2, dp) || (match && helper(s, p, idx1+1, idx2, dp)));
         }
 
         if(match){
-            return helper(s, p, idx1+1, idx2+1);
+            return dp[idx1][idx2] = helper(s, p, idx1+1, idx2+1, dp);
         }
 
-        return false;
+        return dp[idx1][idx2] = false;
     }
 }
