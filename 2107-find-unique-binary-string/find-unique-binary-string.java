ca11702236054053752d54b93a614ed6a5d110e3
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-03-08 22:27:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (76.95%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,23 +1,16 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
         int n = nums.length;
-        if(n==1) return nums[0].equals("0")?"1":"0";
-        String needed = "10";
-        HashSet<String> set = new HashSet<>();
-        for(String s: nums){
-            set.add(s);
-        }
-        return helper(set, n, "");
-    }
 
-    static String helper(HashSet<String> set, int n, String temp){
-        if(temp.length() == n){
-            if(!set.contains(temp)) return temp;
-            return null;
+        StringBuilder res = new StringBuilder();
+
+        for(int i = 0; i < n; i++){
+            char c = nums[i].charAt(i);
+
+            if(c == '0') res.append('1');
+            else res.append('0');
         }
 
-        String left = helper(set, n, temp+"0");
-        if(left != null) return left;
-        return helper(set, n,temp+"1");
+        return res.toString();
     }
 }
