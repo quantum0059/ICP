797d22b545a3c7a78df6ef5f303c3fb65ab9f132
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-03-11 12:37:23 +0530

    Time: 14 ms (69.22%) | Memory: 47.6 MB (80.28%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,24 +1,29 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer,Integer> freq = new HashMap<>();
 
-        for(int i: nums){
-            map.put(i, map.getOrDefault(i, 0)+1);
+        for(int n : nums){
+            freq.put(n, freq.getOrDefault(n,0)+1);
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(b)-map.get(a));
-        for(int i: map.keySet()){
-            
-            pq.offer(i);
-            
+        TreeMap<Integer, List<Integer>> tree = new TreeMap<>(Collections.reverseOrder());
+
+        for(int i : freq.keySet()){
+            int f = freq.get(i);
+            tree.computeIfAbsent(f, x -> new ArrayList<>()).add(i);
         }
-        int arr[] = new int[k];
-        for(int i=0;i<k;i++){
-           arr[i] = pq.poll();
+        
+        int [] arr = new int[k];
+        int j = 0;
+
+        for(int i : tree.keySet()){
+            for(int num : tree.get(i)){
+                if(j == k) break;
+                arr[j++] = num;
+            }
+            if(j == k) break;
         }
 
         return arr;
-
-
     }
 }
