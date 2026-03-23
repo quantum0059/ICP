efe5854c68bf282fb91f6655f9ee0047430f76dc
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-03-23 22:00:56 +0530

    Time: 54 ms (40.67%) | Memory: 47.7 MB (54.63%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -2,29 +2,27 @@ class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum = (Arrays.stream(nums).sum());
-
-        Boolean[][] dp = new Boolean[n][(sum/2)+1];
-
         if(sum%2 != 0) return false;
 
+        boolean[][] dp = new boolean[n+1][(sum/2)+1];
 
-        return helper(nums, n-1, sum/2, dp);
-
-
-    }
-    static boolean helper(int[] nums, int idx, int sum, Boolean[][] dp){
-        if(sum == 0) return true;
-        if(idx<0){
-            return false;
+        for(int i=0;i<=n;i++){
+            dp[i][0] = true;
         }
-        if(dp[idx][sum] != null) return dp[idx][sum];
-        boolean take = false;
-        if(sum>=nums[idx]){
-            take = helper(nums, idx-1, sum-nums[idx], dp);
-        }
-        boolean notTake = helper(nums, idx-1, sum, dp);
-        
 
-        return dp[idx][sum] = (notTake || take);
+        for(int idx=1;idx<=n;idx++){
+            for(int s=1;s<=(sum/2);s++){
+                boolean notTake = dp[idx-1][s];
+                boolean take = false;
+                if(s>=nums[idx-1]){
+                    take = dp[idx-1][s-nums[idx-1]];
+                }
+                
+
+                dp[idx][s] = (take||notTake);
+            }
+        }
+
+        return dp[n][sum/2];
     }
 }
