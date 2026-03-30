4ba0df8d8dda73d880249a47c454e99e139c59e0
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-03-30 11:50:51 +0530

    Time: 24 ms (32.25%) | Memory: 53.8 MB (82.78%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,22 +1,33 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
+        int n = text1.length();
+        int m = text2.length();
 
-        char[] a = text1.toCharArray();
-        char[] b = text2.toCharArray();
-
-        int[][] dp = new int[m+1][n+1];
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (a[i-1] == b[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
+        int[][] dp = new int[n][m];
+        for(int[] arr:dp){
+            Arrays.fill(arr, -1);
         }
-        return dp[m][n];
+
+        return helper(text1, text2, n-1, m-1, dp);
+    }
+
+    static int helper(String text1, String text2, int idx1, int idx2, int[][] dp){
+        if(idx1 < 0 || idx2<0){
+            return 0;
+        }
+
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+
+        int take = 0;
+        int notTake1=0;
+        int notTake2 =0;
+        if(text1.charAt(idx1) == text2.charAt(idx2)){
+            take = 1+helper(text1, text2, idx1-1, idx2-1, dp);
+        }else{
+              notTake1 = helper(text1, text2, idx1-1, idx2, dp);
+              notTake2 = helper(text1, text2, idx1, idx2-1, dp);
+        }
+
+        return dp[idx1][idx2] = take+Math.max(notTake1, notTake2);
     }
 }
