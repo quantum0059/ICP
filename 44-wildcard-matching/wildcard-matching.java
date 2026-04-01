9db975346854d81bd126d99390e819a38f4e2fbe
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-04-01 23:29:57 +0530

    Time: 59 ms (13.28%) | Memory: 47.6 MB (49.90%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -3,16 +3,39 @@ class Solution {
         int n = s.length();
         int m = p.length();
 
-        int[][] dp = new int[n][m];
+        boolean[][] dp = new boolean[n+1][m+1];
 
-        for(int[] arr: dp){
-            Arrays.fill(arr, -1);
+        for(int i=1;i<=n;i++){
+            dp[i][0] = false;
+        }
+        dp[0][0] = true;
+        for(int i=1;i<=m;i++){
+            if(p.charAt(i-1) == '*'){
+                dp[0][i] = dp[0][i-1];
+            }
+            else{
+                dp[0][i] = false;
+            }
+        }
+        
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                boolean ans = false;
+                if(p.charAt(j-1) == '?' || p.charAt(j-1) == s.charAt(i-1)){
+                   ans  = dp[i-1][j-1];
+                }else if(p.charAt(j-1) == '*'){
+                   ans = (dp[i][j-1] || dp[i-1][j]);
+                }
+
+                dp[i][j] = ans;
+            }
         }
 
-        return helper(s, p, n-1, m-1, dp);
+        return dp[n][m];
     }
 
-    static boolean helper(String s, String p, int idx1, int idx2, int[][] dp){
+    static boolean helper(String s, String p, int idx1, int idx2, Boolean[][] dp){
        if(idx1<0 && idx2<0) return true;
        if(idx2<0) return false;
        if(idx1<0){
@@ -22,8 +45,8 @@ class Solution {
         return true;
        }
 
-       if(dp[idx1][idx2] != -1){
-          return dp[idx1][idx2] == 1;
+       if(dp[idx1][idx2] != null){
+          return dp[idx1][idx2] ;
        }
        boolean one = false;
        if(p.charAt(idx2) == '?' || p.charAt(idx2) == s.charAt(idx1)){
@@ -32,8 +55,7 @@ class Solution {
           one = (helper(s, p, idx1, idx2-1, dp) || helper(s, p, idx1-1, idx2, dp));
        } 
 
-       dp[idx1][idx2] = one?1:0;
+       return dp[idx1][idx2] = one;
 
-       return one;
     }
 }
