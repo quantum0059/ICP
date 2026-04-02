56110ca1c0389728ff07409da566dd9a4190159b
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-04-03 00:52:01 +0530

    Time: 45 ms (41.21%) | Memory: 46 MB (86.87%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -19,17 +19,4 @@ class Solution {
         return max;
         
     }
-
-    static int helper(int[] nums, int curr, int prev, int[][] dp){
-        if(curr >= nums.length) return 0;
-        if(prev != -1 && dp[curr][prev]!=-1) return dp[curr][prev];
-        int count = 0;
-        if(prev == -1 || nums[prev] < nums[curr]){
-            count = 1+helper(nums, curr+1, curr, dp);
-        }
-
-        int skip = helper(nums, curr+1, prev, dp);
-
-        return prev!= -1?dp[curr][prev]=Math.max(count, skip): Math.max(count, skip);
-    }
 }
