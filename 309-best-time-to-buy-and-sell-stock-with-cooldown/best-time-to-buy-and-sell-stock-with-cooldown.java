e3b2b3fdc2968c065de4002eca8c07e71798a974
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-04-02 15:16:06 +0530

    Time: 1 ms (70.75%) | Memory: 43.6 MB (39.49%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,37 +1,31 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][2];
+        int[][]dp = new int[n][2];
         
-        for(int[][] arr: dp){
-            for(int[] a: arr){
-                Arrays.fill(a, -1);
-            }
+        for(int[] arr: dp){
+                Arrays.fill(arr, -1);
         }
-            return helper(prices, 0, 0, 1, dp);
+            return helper(prices, 0, 0, dp);
     }
-    static int helper(int[] prices, int idx, int state, int coolDown, int[][][] dp){
-        if(idx==prices.length) return 0;
+    static int helper(int[] prices, int idx, int state, int[][] dp){
+        if(idx>=prices.length) return 0;
 
-        if(dp[idx][state][coolDown] != -1){
-            return dp[idx][state][coolDown];
-        }
-
-        if(coolDown == 0) {
-            return dp[idx][state][coolDown] = helper(prices, idx+1, state, 1, dp);
+        if(dp[idx][state] != -1){
+            return dp[idx][state];
         }
         
         int profit = 0;
         if(state == 0){
-           int buy = -prices[idx]+helper(prices, idx+1, 1, coolDown, dp);
-           int skip = helper(prices, idx+1, 0, coolDown, dp);
+           int buy = -prices[idx]+helper(prices, idx+1, 1, dp);
+           int skip = helper(prices, idx+1, 0, dp);
            profit = Math.max(buy, skip);
         }else{
-            int sell = prices[idx]+helper(prices, idx+1, 0, 0, dp);
-            int skip = helper(prices, idx+1, 1, coolDown, dp);
+            int sell = prices[idx]+helper(prices, idx+2, 0,dp);
+            int skip = helper(prices, idx+1, 1, dp);
             profit = Math.max(sell, skip);
         }
 
-        return dp[idx][state][coolDown] = profit;
+        return dp[idx][state] = profit;
     }
 }
