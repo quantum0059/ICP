ceef067065052f49303769c969b9a3ad0ea2cd9c
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-04-07 00:17:10 +0530

    Time: 20 ms (70.42%) | Memory: 44.1 MB (96.98%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,21 +1,30 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
+        int c = cuts.length;
         ArrayList<Integer> list = new ArrayList<>();
-        // Arrays.sort(cuts);
         list.add(0);
         list.add(n);
-        // list.addAll(Arrays.asList(cuts));
         for(int i: cuts){
             list.add(i);
         }
         Collections.sort(list);
-        int[][] dp = new int[list.size()][list.size()];
-        for(int[] arr: dp){
-            Arrays.fill(arr, -1);
+        int[][] dp = new int[c+2][c+2];
+
+        for(int i=c;i>=1;i--){
+            for(int j=1;j<=c;j++){
+                if(i>j) continue;
+                int min = Integer.MAX_VALUE;
+                for(int k=i;k<=j;k++){
+                    int cost = list.get(j+1)-list.get(i-1)+dp[i][k-1]+dp[k+1][j];
+                    min = Math.min(cost, min);
+                }
+                dp[i][j] = min;
+            }
         }
+        
 
 
-        return helper(1, cuts.length, list, dp);
+        return dp[1][cuts.length];
     }
     static int helper(int i, int j, ArrayList<Integer> list, int[][] dp){
         if(i>j) return 0;
