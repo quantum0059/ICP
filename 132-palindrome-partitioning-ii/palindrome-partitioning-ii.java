276277c5f8681ed59c00739a001ea8d06550170f
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-04-08 12:51:57 +0530

    Time: 2524 ms (10.57%) | Memory: 42.8 MB (86.33%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+
+        Arrays.fill(dp, -1);
+        return helper(0, n, s, dp)-1;
+
+    }
+    static int helper(int i, int n, String s, int[] dp){
+        if(i==n) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        int minCuts = Integer.MAX_VALUE;
+
+        for(int k=i;k<n;k++){
+            if(isPalindrome(i, k, s)){
+               int cuts = 1+helper(k+1, n, s, dp);
+               minCuts = Math.min(minCuts, cuts);
+            }
+        }
+
+        return dp[i] = minCuts;
+    }
+    static boolean isPalindrome(int i, int j, String s){
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+
+    }
+}
