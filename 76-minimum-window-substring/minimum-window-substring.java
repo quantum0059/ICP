47e8a5b8c06306778573a35b32e11c40046edf27
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-05-21 15:36:51 +0530

    Time: 16 ms (60.80%) | Memory: 47.3 MB (42.87%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,20 +1,18 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-
         HashMap<Character, Integer> map = new HashMap<>();
 
         for(char c: t.toCharArray()){
             map.put(c, map.getOrDefault(c, 0)+1);
         }
+        int m = t.length();
 
         int st = 0;
-        int startingIdx = -1;
         int count = 0;
         int minLength = Integer.MAX_VALUE;
-        int r = 0;
-        while(r<s.length()){
+        int startingIdx = -1;
+
+        for(int r=0;r<s.length();r++){
             char c = s.charAt(r);
             if(map.containsKey(c) && map.get(c)>0){
                 count++;
@@ -22,23 +20,17 @@ class Solution {
             map.put(c, map.getOrDefault(c, 0)-1);
 
             while(count == m){
-                if(r-st+1<minLength){
+                if(minLength > r-st+1){
                     minLength = r-st+1;
                     startingIdx = st;
                 }
 
-                char a = s.charAt(st);
-                map.put(a, map.getOrDefault(a, 0)+1);
-                if(map.get(a) > 0){
-                    count--;
-                }
+                map.put(s.charAt(st), map.get(s.charAt(st))+1);
+                if(map.get(s.charAt(st))>0) count--;
                 st++;
             }
-            r++;
         }
 
-        
-        return startingIdx == -1 ? "" : s.substring(startingIdx,startingIdx+minLength);
-
+        return startingIdx == -1 ? "": s.substring(startingIdx, startingIdx+minLength);
     }
 }
