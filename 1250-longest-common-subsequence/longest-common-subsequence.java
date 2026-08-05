271ecbef17bf551451a43656d058d4d9d2c302b9
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-08-05 08:51:58 +0530

    Time: 29 ms (17.11%) | Memory: 53.8 MB (70.07%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,44 +3,29 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
 
-        int[][] dp = new int[n+1][m+1];
-        
-        for(int idx1 = 1;idx1<=n;idx1++){
-            for(int idx2=1;idx2<=m;idx2++){
-                int take = 0;
-                int notTake1=0;
-                int notTake2 =0;
-                if(text1.charAt(idx1-1) == text2.charAt(idx2-1)){
-                    take = 1+dp[idx1-1][idx2-1];
-                }else{
-                    notTake1 = dp[idx1-1][idx2];
-                    notTake2 = dp[idx1][idx2-1];
-                }
+        int[][] dp = new int[n][m];
 
-                dp[idx1][idx2]  = take+Math.max(notTake1, notTake2);
-            }
+        for(int[] arr: dp){
+            Arrays.fill(arr, -1);
         }
 
-        return dp[n][m];
+        return helper(dp, text1, text2, n, m, 0, 0);
     }
 
-    static int helper(String text1, String text2, int idx1, int idx2, int[][] dp){
-        if(idx1 < 0 || idx2<0){
+    int helper(int[][] dp, String text1, String text2, int n, int m, int idx1, int idx2){
+        if(idx1>=n || idx2>=m){
             return 0;
         }
-
         if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
-
-        int take = 0;
-        int notTake1=0;
-        int notTake2 =0;
+        int take=0, skip1=0, skip2 = 0;
+        
         if(text1.charAt(idx1) == text2.charAt(idx2)){
-            take = 1+helper(text1, text2, idx1-1, idx2-1, dp);
+           take = 1+helper(dp, text1, text2, n, m, idx1+1, idx2+1);
         }else{
-              notTake1 = helper(text1, text2, idx1-1, idx2, dp);
-              notTake2 = helper(text1, text2, idx1, idx2-1, dp);
+           skip1 = helper(dp, text1, text2, n, m, idx1+1, idx2);
+           skip2 = helper(dp, text1, text2, n, m, idx1, idx2+1);
         }
 
-        return dp[idx1][idx2] = take+Math.max(notTake1, notTake2);
+        return dp[idx1][idx2] = Math.max(take, Math.max(skip1, skip2));
     }
 }
