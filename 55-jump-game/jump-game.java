4a86776867ce0d707902129b3fcc89ad4f134a61
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-08-07 22:13:47 +0530

    Time: 354 ms (5.37%) | Memory: 48.3 MB (8.65%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,13 +1,25 @@
 class Solution {
+    Boolean[] dp;
     public boolean canJump(int[] nums) {
-        int goal = nums.length - 1;
+        int n = nums.length;
+        dp = new Boolean[n]; 
+        return helper(nums, n, 0);
+    }
 
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (i + nums[i] >= goal) {
-                goal = i;
-            }
+    boolean helper(int[] nums, int n , int idx){
+        if(idx>=n){
+            return false;
+        }
+        if(idx == n-1) return true;
+        if(dp[idx] != null) return dp[idx];
+        int limit = nums[idx];
+        
+        for(int i=1;i<=limit;i++){
+          if(helper(nums, n, idx+i)){
+            return dp[idx] = true;
+          }
         }
 
-        return goal == 0;        
+        return dp[idx] = false;
     }
 }
