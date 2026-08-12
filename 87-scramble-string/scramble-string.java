872a4b1440cbc3280780201b805c2baa57843805
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-08-12 09:36:01 +0530

    Time: 13 ms (26.08%) | Memory: 44.7 MB (98.14%) - LeetSync

--- 87-scramble-string/scramble-string.java
+++ 87-scramble-string/scramble-string.java
@@ -1,32 +1,25 @@
 class Solution {
-    Map<String, Boolean> map;
     public boolean isScramble(String s1, String s2) {
-        map = new HashMap<>();
-        return helper(s1, s2);
-    }
-
-    boolean helper(String s1, String s2){
-        if(s1.equals(s2)) return true;
-
-        if(s1.length() != s2.length()) return false;
-        String key = s1+" "+s2;
-        if(map.get(key) != null) return map.get(key);
-
-        boolean result = false;
         int n = s1.length();
-
-        for(int i=1;i<n;i++){
-            boolean swapped = helper(s1.substring(0, i), s2.substring(0, i)) && helper(s1.substring(i), s2.substring(i));
-            boolean notSwapped = helper(s1.substring(i, n), s2.substring(0, n-i)) && helper(s1.substring(0, i), s2.substring(n-i, n));
-
-            if(swapped || notSwapped){
-                result = true;
-                break;
+        boolean dp[][][] = new boolean[n + 1][n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[1][i][j] = s1.charAt(i) == s2.charAt(j);
             }
-
         }
-
-         map.put(key, result);
-         return map.get(key);
+        for (int length = 2; length <= n; length++) {
+            for (int i = 0; i < n + 1 - length; i++) {
+                for (int j = 0; j < n + 1 - length; j++) {
+                    for (int newLength = 1; newLength < length; newLength++) {
+                        boolean dp1[] = dp[newLength][i];
+                        boolean dp2[] = dp[length - newLength][i + newLength];
+                        dp[length][i][j] |= dp1[j] && dp2[j + newLength];
+                        dp[length][i][j] |=
+                        dp1[j + length - newLength] && dp2[j];
+                    }
+                }
+            }
+        }
+        return dp[n][0][0];
     }
 }
