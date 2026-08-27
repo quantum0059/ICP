2784b703bf9051b128d3770fc36d469ebf704a04
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-08-27 15:51:43 +0530

    Time: 23 ms (68.40%) | Memory: 53.8 MB (61.94%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,6 +1,6 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
-         int n = str1.length();
+        int n = str1.length();
         int m = str2.length();
 
         int[][] dp = new int[n+1][m+1];
@@ -22,10 +22,10 @@ class Solution {
 
         while(i>0 && j>0){
             if(str1.charAt(i-1) == str2.charAt(j-1)){
-               sb.append(str1.charAt(i-1));
-               i--;
-               j--;
-            }else if(dp[i-1][j]> dp[i][j-1]){
+                sb.append(str1.charAt(i-1));
+                i--;
+                j--;
+            }else if(dp[i-1][j] > dp[i][j-1]){
                 sb.append(str1.charAt(i-1));
                 i--;
             }else{
@@ -38,6 +38,7 @@ class Solution {
             sb.append(str1.charAt(i-1));
             i--;
         }
+
         while(j>0){
             sb.append(str2.charAt(j-1));
             j--;
