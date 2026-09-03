9ff2cecbe93f881ca0c0b3f2d214fd6062cd78d2
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-09-03 18:35:03 +0530

    Time: 2286 ms (32.51%) | Memory: 42.4 MB (98.46%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,41 +1,33 @@
 class Solution {
     public int minCut(String s) {
         int n = s.length();
-        int[] dp = new int[n+1];
 
-        for(int i=n-1;i>=0;i--){
-            int minCuts = Integer.MAX_VALUE;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
 
-            for(int k=i;k<n;k++){
-                if(isPalindrome(i, k, s)){
-                    int cuts = 1+dp[k+1];
-                    minCuts = Math.min(minCuts, cuts);
-                }
-            }
-            dp[i] = minCuts;
-        }
-        return dp[0]-1;
-
+        return helper(s, 0, n-1, dp)-1;
     }
-    static int helper(int i, int n, String s, int[] dp){
-        if(i==n) return 0;
+    int helper(String s, int i, int n, int[] dp){
+        if(i>n) return 0;
 
         if(dp[i] != -1) return dp[i];
 
         int minCuts = Integer.MAX_VALUE;
 
-        for(int k=i;k<n;k++){
-            if(isPalindrome(i, k, s)){
-               int cuts = 1+helper(k+1, n, s, dp);
-               minCuts = Math.min(minCuts, cuts);
+        for(int k=i;k<=n;k++){
+            if(isPalindrome(s, i, k)){
+                int count = 1+helper(s, k+1, n, dp);
+                minCuts = Math.min(minCuts, count);
             }
         }
-
         return dp[i] = minCuts;
     }
-    static boolean isPalindrome(int i, int j, String s){
+
+    boolean isPalindrome(String s, int i, int j){
         while(i<j){
-            if(s.charAt(i) != s.charAt(j)) return false;
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
             i++;
             j--;
         }
