b7404abc824a1d9ff9d65864a705afe2320c8eb1
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-09-06 23:39:43 +0530

    Time: 50 ms (5.20%) | Memory: 58.2 MB (9.68%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,23 +1,50 @@
 class Solution {
-    public int numDistinct(String s, String t) {
-        int n = s.length();
-        int m = t.length();
+    static int[][] dp;
 
-        int[][] dp = new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
+    public int numDistinct(String s, String t) {
+        dp = new int[s.length() + 1][t.length() + 1];
+        for (int i = 0; i <= s.length(); i++) {
+            Arrays.fill(dp[i], -1);
         }
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s.charAt(i-1) == t.charAt(j-1)){
-                    dp[i][j] =  dp[i-1][j-1]+dp[i-1][j];
-                } else{
-                    dp[i][j] = dp[i-1][j];
-                }
+        StringBuilder sb = new StringBuilder();
+        return helper(s, t, 0, sb);
+    }
+
+    static int helper(String s, String t, int idx, StringBuilder sb) {
+
+        if (sb.length() > 0) {
+            int last = sb.length() - 1;
+            if (sb.charAt(last) != t.charAt(last)) {
+                return 0;
             }
         }
 
-        return dp[n][m];
+        
+        if (dp[idx][sb.length()] != -1) {
+            return dp[idx][sb.length()];
+        }
+
+        
+        if (idx == s.length()) {
+            return dp[idx][sb.length()] =
+                    (sb.length() == t.length()) ? 1 : 0;
+        }
+
+        if (sb.length() == t.length()) {
+            return dp[idx][sb.length()] = 1;
+        }
+
+        int count = 0;
+
+       
+        sb.append(s.charAt(idx));
+        count += helper(s, t, idx + 1, sb);
+        sb.deleteCharAt(sb.length() - 1);
+
+        
+        count += helper(s, t, idx + 1, sb);
+
+        return dp[idx][sb.length()] = count;
     }
 }
