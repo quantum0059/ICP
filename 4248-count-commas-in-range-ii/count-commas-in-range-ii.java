8b43bfbceccbb262a603a3f38b7085d7e1b1c4be
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-09-10 00:38:18 +0530

    Time: 1 ms (99.36%) | Memory: 42.6 MB (71.15%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+        long start = 1000;
+        int commas = 1;
+
+        while(start<=n){
+            long end = start*1000-1;
+
+            long count = Math.min(n, end)-start+1;
+            if(count >0){
+                ans+=count*commas;
+            }
+
+            start*=1000;
+            commas++;
+        }
+        return ans;
+    }
+}
