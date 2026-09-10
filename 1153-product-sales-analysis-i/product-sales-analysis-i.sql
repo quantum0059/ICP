3936e0533ce261080ca805bf3d5e601c29330f69
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-09-10 17:46:11 +0530

    Time: 1202 ms (89.05%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,7 +1,6 @@
-# Write your MySQL query statement below
-SELECT Product.product_name,
-Sales.year,
-Sales.price
-FROM Sales
-JOIN Product
-ON Sales.product_id = Product.product_id;
+# Write your MySQL query statement be
+SELECT p.product_name, s.year, s.price
+FROM Sales as s
+LEFT JOIN 
+Product as p
+on s.product_id = p.product_id;
