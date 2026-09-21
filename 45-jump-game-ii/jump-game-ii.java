97602768400f97a36095762fe5aa07eef96a8e7d
Shivam Sharma <shivamsharma.anva@gmail.com>
2026-09-21 15:19:51 +0530

    Time: 1 ms (99.79%) | Memory: 47.4 MB (21.37%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,25 +1,20 @@
 class Solution {
-    int[] dp;
     public int jump(int[] nums) {
-        int n = nums.length;
-        if(n==1) return 0;
-        dp = new int[n];
-        Arrays.fill(dp, -1) ;
-        return helper(nums, n, 0);
-    }
 
-    int helper(int[] nums, int n , int idx){
-        if(idx>=n){
-            return Integer.MAX_VALUE/2;
-        }
-        if(idx == n-1) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        int limit = nums[idx];
-        int count = Integer.MAX_VALUE/2;
-        for(int i=1;i<=limit;i++){
-          count = Math.min(count, 1+helper(nums, n, idx+i));
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
         }
 
-        return dp[idx] = count;
+        return jumps;
     }
 }
